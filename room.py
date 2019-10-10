07368ef8e20317fdc17124964edfbc27bef1ab61
JontAmazon <jonatan.lindholm10@gmail.com>
2019-10-10 10:58:19 +0200

    Changed function name from room_omega_X to init_roomX. Implemented half of init_room2().

--- room.py
+++ room.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import scipy.linalg as sl
 
 
 class Room(object):
@@ -19,12 +20,13 @@ class Room(object):
         self.u_km1 = None #used for relaxation.
 
         assert (room < 4),'The rank is too high, you might be trying to initiate too many instances'
+        #Initializes A and b for room 1, 2 or 3:
         if room == 1:
-            self.room_omega_1()
+            self.init_room1()
         elif room == 2:
-            self.room_omega_2()
+            self.init_room2()
         else:
-            self.room_omega_3()
+            self.init_room3()
 
 """
     Since A and b are partly constant throughout the
@@ -33,24 +35,63 @@ class Room(object):
     A and b are then updated in every iteration, as the vectors gamma1 and gamma2 change.
     This is done in the solve() function.
 """
-    def room_omega_1(self):
+    def init_room1(self):
         self.A = 'hej'
-        
-        # A=
-        # B =
-        # U = uvektorn för rum 1
-        return A,b
-    def room_omega_2(self):
-        self.A = 'hej'
-        # A=
-        # B =
 
-        return A,b,U
-    def room_omega_3(self):
+        
+    def init_room2(self):
+        """ Builds and returns the matrices A and b for room 2. """
+        height = 2                     #heigth of the room
+        width = 1                      #width om the room
+        M = int(round(height/dx)) - 1  #number of rows of nodes
+        N = int(round(width/dx)) - 1   #number of cols of nodes
+        size = M*N
+        
+        # [Building A].
+        # The bulk of A is very close to a toeplitz matrix with 5 diagonals.
+        first_row = np.zeros(size)
+        first_row[0] = -4
+        first_row[1] = 1
+        first_row[N] = 1
+        self.A = sl.toeplitz(first_row, first_row)
+        
+        # The two inner super- and subdiagonals of this toeplitz matrix need to
+        # be modified. Specifically, every N:th element should be set to zero,
+        # for a total of (M-1) times, since our grid has M rows, and only the
+        # first element to be set to zero goes "outside" of the matrix A.
+        #
+        # SUB: first zero goes in row N.
+        row = N
+        for i in range(M-1):
+            self.A[row, row-1] = 0
+            row += N
+        
+        #SUPER: first zero goes in row N-1.
+        row = N-1
+        for i in range(M-1):
+            self.A[row, row+1] = 0
+            row += N
+        
+        
+        # [Building b].
+        # Room 2 has 6 different (Dirichlet) boundary conditions. 2 of these
+        # change in every iteration, while 4 are constant.
+        # here we focus on these 4...
+        # todo continue here (Jonnte)
+        
+        
+        
+        
+        
+                
+        
+        
+        
+
+    def init_room3(self):
         self.A = 'hej'
-        # A=
-        # B =
-        return A,b,U
+
+
 
 """        
     Note that the vectors gamma1 and gamma2 store different things at
