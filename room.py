bd142825466eb86078a1fd81f6f8fa0e5c99a1b2
JontAmazon <jonatan.lindholm10@gmail.com>
2019-10-14 11:44:23 +0200

    Changed mistake at room 2's lower left corner. Now correct index for this in update2()

--- room.py
+++ room.py
@@ -212,7 +212,7 @@ class Room(object):
         # The above code has overwritten the b-values at two corners. There, the b-vector 
         # should only have contributions from the outer wall:
         self.b[N-1] -= self.heater_temp
-        self.b[N**2-N] -= self.window_temp
+        self.b[-N] -= self.window_temp
 
 
 
